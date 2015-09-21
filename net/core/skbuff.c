ebfd0305d23ae8bf7728d4ffce63aa5cc57a60c2
vdhoki <quic_vdhoki@quicinc.com>
2015-09-21 18:50:13 -0500

    net: skbuff: use debug objects to track skb allocations
    * tracks skb allocations and frees and warns / errors if
      re-use occurs
    * init/destroy for slab allocations
    * activate/deactivate for in use.
    
    Change-Id: I4adc70570747fa5aec577e4156d1604e21194c42
    Signed-off-by: vdhoki <quic_vdhoki@quicinc.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -121,6 +121,7 @@ struct kmem_cache *skb_data_cache;
 #endif
 
 #include "skbuff_recycle.h"
+#include "skbuff_debug.h"
 
 static struct kmem_cache *skbuff_fclone_cache __ro_after_init;
 #ifdef CONFIG_SKB_EXTENSIONS
@@ -398,6 +399,7 @@ static inline void __finalize_skb_around(struct sk_buff *skb, void *data,
 	atomic_set(&shinfo->dataref, 1);
 
 	skb_set_kcov_handle(skb, kcov_common_handle());
+	skbuff_debugobj_init_and_activate(skb);
 }
 
 static inline void *__slab_build_skb(struct sk_buff *skb, void *data,
@@ -720,6 +722,7 @@ struct sk_buff *__alloc_skb(unsigned int size, gfp_t gfp_mask,
 		refcount_set(&fclones->fclone_ref, 1);
 	}
 
+	skbuff_debugobj_init_and_activate(skb);
 	return skb;
 
 nodata:
@@ -1159,6 +1162,7 @@ void kfree_skbmem(struct sk_buff *skb)
 
 	switch (skb->fclone) {
 	case SKB_FCLONE_UNAVAILABLE:
+		skbuff_debugobj_deactivate(skb);
 		kmem_cache_free(skbuff_cache, skb);
 		return;
 
@@ -1180,6 +1184,7 @@ void kfree_skbmem(struct sk_buff *skb)
 	if (!refcount_dec_and_test(&fclones->fclone_ref))
 		return;
 fastpath:
+	skbuff_debugobj_deactivate(&fclones->skb1);
 	kmem_cache_free(skbuff_fclone_cache, fclones);
 }
 
@@ -2133,6 +2138,7 @@ struct sk_buff *skb_clone(struct sk_buff *skb, gfp_t gfp_mask)
 			return NULL;
 
 		n->fclone = SKB_FCLONE_UNAVAILABLE;
+		skbuff_debugobj_init_and_activate(n);
 	}
 
 	return __skb_clone(n, skb);
@@ -5951,6 +5957,7 @@ void kfree_skb_partial(struct sk_buff *skb, bool head_stolen)
 	if (head_stolen) {
 		skb_release_head_state(skb);
 		kmem_cache_free(skbuff_cache, skb);
+		skbuff_debugobj_deactivate(skb);
 	} else {
 		__kfree_skb(skb);
 	}
