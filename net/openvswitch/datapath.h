b3c89ffa6f7c8d0f6baa88e633114b4758398bcf
Suman Ghosh <sumaghos@codeaurora.org>
2020-11-04 13:34:19 +0530

    openvswitch: Update function to search OVS flow rule by MAC address for Multicast
    Update function to search OVS flow rule by MAC address for Multicast.
    
    Change-Id: Ie8f65c22dd7578a9a09370a10f67333228d45722
    Signed-off-by: Pavithra R <quic_pavir@quicinc.com>
    Signed-off-by: Utkrisht Sikka <quic_usikka@quicinc.com>
    Signed-off-by: Suman Ghosh <sumaghos@codeaurora.org>

--- net/openvswitch/datapath.h
+++ net/openvswitch/datapath.h
@@ -315,8 +315,9 @@ struct net_device *ovs_accel_egress_dev_find(void *dp_inst,
 					     struct sk_buff *skb);
 
 struct sw_flow *ovs_accel_flow_find_by_mac(void *dp_inst,
-					   struct net_device *br_dev,
-					   u8 *smac, u8 *dmac, u16 type);
+						struct net_device *indev,
+						struct net_device *outdev,
+						u8 *smac, u8 *dmac, uint16_t type);
 
 /* 'KEY' must not have any bits set outside of the 'MASK' */
 #define OVS_MASKED(OLD, KEY, MASK) ((KEY) | ((OLD) & ~(MASK)))
