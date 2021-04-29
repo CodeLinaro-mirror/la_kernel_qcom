7b29068a2cbd422264b8f4372719d8cb6bca7df8
Kathiravan T <kathirav@codeaurora.org>
2021-04-29 16:29:02 +0530

    net: skbuff: cache for skb->data for LM profiles
    Cache for skb->data for LM profiles .
    
    Signed-off-by: Kathiravan T <kathirav@codeaurora.org>
    Change-Id: I7bcac8d05a11aa3d1a9e015d6530562ed81477a0
    Signed-off-by: Mohan Giridhar Addepalli<quic_gaddepal@quicinc.com>
    Signed-off-by: Medha <quic_medhlnu@quicinc.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -90,6 +90,12 @@
 #include "sock_destructor.h"
 
 struct kmem_cache *skbuff_cache __ro_after_init;
+
+#if defined(CONFIG_SKB_FIXED_SIZE_2K) && !defined(__LP64__)
+struct kmem_cache *skb_data_cache;
+#define SKB_DATA_CACHE_SIZE	2176
+#endif
+
 static struct kmem_cache *skbuff_fclone_cache __ro_after_init;
 #ifdef CONFIG_SKB_EXTENSIONS
 static struct kmem_cache *skbuff_ext_cache __ro_after_init;
@@ -584,15 +590,27 @@ static void *kmalloc_reserve(unsigned int *size, gfp_t flags, int node,
 	 * Try a regular allocation, when that fails and we're not entitled
 	 * to the reserves, fail.
 	 */
-	obj = kmalloc_node_track_caller(obj_size,
-					flags | __GFP_NOMEMALLOC | __GFP_NOWARN,
-					node);
+#if defined(CONFIG_SKB_FIXED_SIZE_2K) && !defined(__LP64__)
+	if (size > SZ_2K && size <= SKB_DATA_CACHE_SIZE)
+		obj = kmem_cache_alloc_node(skb_data_cache,
+					    flags | __GFP_NOMEMALLOC | __GFP_NOWARN,
+					    node);
+	else
+#endif
+		obj = kmalloc_node_track_caller(obj_size,
+						flags | __GFP_NOMEMALLOC | __GFP_NOWARN,
+						node);
 	if (obj || !(gfp_pfmemalloc_allowed(flags)))
 		goto out;
 
 	/* Try again but now we are using pfmemalloc reserves */
 	ret_pfmemalloc = true;
-	obj = kmalloc_node_track_caller(obj_size, flags, node);
+#if defined(CONFIG_SKB_FIXED_SIZE_2K) && !defined(__LP64__)
+	if (size > SZ_2K && size <= SKB_DATA_CACHE_SIZE)
+		obj = kmem_cache_alloc_node(skb_data_cache, flags, node);
+	else
+#endif
+		obj = kmalloc_node_track_caller(obj_size, flags, node);
 
 out:
 	if (pfmemalloc)
@@ -4843,6 +4861,13 @@ static void skb_extensions_init(void) {}
 
 void __init skb_init(void)
 {
+#if defined(CONFIG_SKB_FIXED_SIZE_2K) && !defined(__LP64__)
+	skb_data_cache = kmem_cache_create_usercopy("skb_data_cache",
+						    SKB_DATA_CACHE_SIZE,
+						    0, 0, 0, SKB_DATA_CACHE_SIZE,
+						    NULL);
+#endif
+
 	skbuff_cache = kmem_cache_create_usercopy("skbuff_head_cache",
 					      sizeof(struct sk_buff),
 					      0,
@@ -4861,12 +4886,12 @@ void __init skb_init(void)
 	 * and should not be copied to/from user.
 	 */
 	skb_small_head_cache = kmem_cache_create_usercopy("skbuff_small_head",
-						SKB_SMALL_HEAD_CACHE_SIZE,
-						0,
-						SLAB_HWCACHE_ALIGN | SLAB_PANIC,
-						0,
-						SKB_SMALL_HEAD_HEADROOM,
-						NULL);
+							  SKB_SMALL_HEAD_CACHE_SIZE,
+							  0,
+							  SLAB_HWCACHE_ALIGN | SLAB_PANIC,
+							  0,
+							  SKB_SMALL_HEAD_HEADROOM,
+							  NULL);
 	skb_extensions_init();
 }
 
