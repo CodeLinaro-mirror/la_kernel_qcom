a1f78378951e8f93d76a9368f814373ab62ee434
Subhash Kumar Katnapally <quic_skatnapa@quicinc.com>
2022-09-06 14:39:55 +0530

    net: OVS: Handle netdev unregister
    Trigger ovsmgr port deletion during openvswitch netdev unregister handling.
    
    Change-Id: If59360cd89a39294876a4b577afd0bbf98212718
    Signed-off-by: Subhash Kumar Katnapally <quic_skatnapa@quicinc.com>
    Signed-off-by: Pavithra R <quic_pavir@quicinc.com>

--- net/openvswitch/datapath.c
+++ net/openvswitch/datapath.c
@@ -288,7 +288,7 @@ static void ovs_dp_port_add_notify(struct datapath *dp, struct vport *vp,
 }
 
 /* Notify datapath port delete event to acceleration callback */
-static void ovs_dp_port_del_notify(struct datapath *dp, struct vport *vp)
+void ovs_dp_port_del_notify(struct datapath *dp, struct vport *vp)
 {
 	struct ovs_accel_callback *ovs_cb;
 
@@ -2817,6 +2817,7 @@ struct net_device *ovs_accel_egress_dev_find(void *dp_inst,
 				goto done;
 
 			dev = vport->dev;
+			dev_hold(dev);
 			rcu_read_unlock();
 			return dev;
 		}
