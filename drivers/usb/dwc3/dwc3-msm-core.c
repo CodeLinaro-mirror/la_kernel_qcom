b18e1d8a9f3410e0879a472b9c88db44b7efbd2b
Prashanth K <prashanth.k@oss.qualcomm.com>
2023-03-29 17:16:06 +0530

    usb: dwc3-msm: Add reboot handler in dwc3 msm
    In some reboot cases, smmmu translations are disabled even
    before usb stack is unloaded. Because of which USB accesses
    address regions which aren't translated, causing NOC errors.
    
    Avoid this by implementing a reboot handler callback which
    removes USB stack before SMMU is disabled.
    
    Change-Id: I242b53f6699077038ba4634c2a43637d2459636d
    Signed-off-by: Prashanth K <prashanth.k@oss.qualcomm.com>
    Signed-off-by: Hiren Gohel <vhiren@qti.qualcomm.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -7027,6 +7027,14 @@ static void dwc3_msm_usb3_phy_poweroff(struct dwc3_msm *mdwc, bool off)
 	}
 }
 
+static void dwc3_msm_shutdown(struct platform_device *pdev)
+{
+	struct dwc3_msm	*mdwc = platform_get_drvdata(pdev);
+
+	dbg_log_string("Entry\n");
+	dwc3_msm_remove(pdev);
+}
+
 static int dwc3_msm_host_ss_powerdown(struct dwc3_msm *mdwc)
 {
 	u32 reg;
@@ -8162,6 +8170,7 @@ MODULE_DEVICE_TABLE(of, of_dwc3_matach);
 static struct platform_driver dwc3_msm_driver = {
 	.probe		= dwc3_msm_probe,
 	.remove		= dwc3_msm_remove,
+	.shutdown	= dwc3_msm_shutdown,
 	.driver		= {
 		.name	= "msm-dwc3",
 		.pm	= &dwc3_msm_dev_pm_ops,
