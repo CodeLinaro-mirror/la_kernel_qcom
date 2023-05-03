764385a50f01804f4d5c3ed6b63e10cc304e6c0d
Ken Zhu <quic_guigenz@quicinc.com>
2023-05-02 21:39:54 -0700

    bridge: fix bridge vlan filter toggle issue
    when vlan filter is disabled, it could cause new bridge
    creation failed if vlan filter setting existing in the
    netlink creation message.
    
    Change-Id: I02f010e746e94623fda544846d05908a83d14fa2
    Signed-off-by: Ken Zhu <quic_guigenz@quicinc.com>

--- net/bridge/br_netlink.c
+++ net/bridge/br_netlink.c
@@ -1317,6 +1317,7 @@ static int br_changelink(struct net_device *brdev, struct nlattr *tb[],
 		br_stp_set_bridge_priority(br, priority);
 	}
 
+#ifdef CONFIG_BRIDGE_VLAN_FILTERING
 	if (data[IFLA_BR_VLAN_FILTERING]) {
 		u8 vlan_filter = nla_get_u8(data[IFLA_BR_VLAN_FILTERING]);
 
@@ -1325,7 +1326,6 @@ static int br_changelink(struct net_device *brdev, struct nlattr *tb[],
 			return err;
 	}
 
-#ifdef CONFIG_BRIDGE_VLAN_FILTERING
 	if (data[IFLA_BR_VLAN_PROTOCOL]) {
 		__be16 vlan_proto = nla_get_be16(data[IFLA_BR_VLAN_PROTOCOL]);
 
