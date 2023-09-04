54fc851797bfbc5534ba8929cd53f34bd1bb3570
Asit Shah <asitshah@qti.qualcomm.com>
2023-09-04 15:44:02 +0530

    net: stmmac: Add dlkm support
    Replace ethqos function calls with function pointer in stmmac.
    Makefile changes to compile all ethqos files as single ko.
    Fix below issue-
    * Fix AXI & AHB clock warning by enabling GDSC
      regulator before clock enable.
    * enable c22 mdio phy addr probe to avoid multiple
      dummy phy address detection.
    * SMMU context fault due to SMMU probe is getting
      triger after the Ethernet probe
      & not storing the smmu context properly.
    * Proper GPIO reset and remove unused variables.
    
    Change-Id: Ie12966d477e41f18aaccb162fd8a4217b7e19a67
    Signed-off-by: Sarosh Hasan <quic_sarohasa@quicinc.com>
    Signed-off-by: Navjyot SINGH <quic_navjsing@quicinc.com>
    Signed-off-by: Asit Shah <asitshah@qti.qualcomm.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-gpio.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-gpio.c
@@ -143,6 +143,7 @@ reg_error:
 	ethqos_disable_regulators(ethqos);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ethqos_init_regulators);
 
 void ethqos_disable_regulators(struct qcom_ethqos *ethqos)
 {
@@ -166,6 +167,7 @@ void ethqos_disable_regulators(struct qcom_ethqos *ethqos)
 		ethqos->gdsc_emac = NULL;
 	}
 }
+EXPORT_SYMBOL_GPL(ethqos_disable_regulators);
 
 void ethqos_free_gpios(struct qcom_ethqos *ethqos)
 {
@@ -173,6 +175,7 @@ void ethqos_free_gpios(struct qcom_ethqos *ethqos)
 		gpio_free(ethqos->gpio_phy_intr_redirect);
 	ethqos->gpio_phy_intr_redirect = -1;
 }
+EXPORT_SYMBOL_GPL(ethqos_free_gpios);
 
 int ethqos_init_pinctrl(struct device *dev)
 {
@@ -225,6 +228,7 @@ int ethqos_init_pinctrl(struct device *dev)
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ethqos_init_pinctrl);
 
 int ethqos_init_gpio(struct qcom_ethqos *ethqos)
 {
@@ -254,3 +258,6 @@ gpio_error:
 	ethqos_free_gpios(ethqos);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(ethqos_init_gpio);
+
+MODULE_LICENSE("GPL");
