93ac2dceee44722d62ed7c3158b5bb2dc364e8c0
Mao Jinlong <quic_jinlmao@quicinc.com>
2023-11-06 22:09:34 -0800

    FROMLIST: coresight: Add remote etm support
    Add support for ETM trace collection on remote processor using
    coreSight framework.
    
    Link: https://patchwork.kernel.org/project/linux-arm-kernel/patch/20231107060939.13449-2-quic_jinlmao@quicinc.com/
    Change-Id: I3c148cc146c3df0f7604289e1a3d885c8035aa98
    Signed-off-by: Mao Jinlong <quic_jinlmao@quicinc.com>

--- drivers/hwtracing/coresight/coresight-sysfs.c
+++ drivers/hwtracing/coresight/coresight-sysfs.c
@@ -155,6 +155,7 @@ static int coresight_validate_source_sysfs(struct coresight_device *csdev,
 	if (subtype != CORESIGHT_DEV_SUBTYPE_SOURCE_PROC &&
 	    subtype != CORESIGHT_DEV_SUBTYPE_SOURCE_SOFTWARE &&
 	    subtype != CORESIGHT_DEV_SUBTYPE_SOURCE_TPDM &&
+	    subtype != CORESIGHT_DEV_SUBTYPE_SOURCE_REMOTE_PROC &&
 	    subtype != CORESIGHT_DEV_SUBTYPE_SOURCE_OTHERS) {
 		dev_err(&csdev->dev, "wrong device subtype in %s\n", function);
 		return -EINVAL;
@@ -231,6 +232,7 @@ int coresight_enable_sysfs(struct coresight_device *csdev)
 		break;
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_SOFTWARE:
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_TPDM:
+	case CORESIGHT_DEV_SUBTYPE_SOURCE_REMOTE_PROC:
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_OTHERS:
 		/*
 		 * Use the hash of source's device name as ID
@@ -282,6 +284,7 @@ void coresight_disable_sysfs(struct coresight_device *csdev)
 		break;
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_SOFTWARE:
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_TPDM:
+	case CORESIGHT_DEV_SUBTYPE_SOURCE_REMOTE_PROC:
 	case CORESIGHT_DEV_SUBTYPE_SOURCE_OTHERS:
 		hash = hashlen_hash(hashlen_string(NULL, dev_name(&csdev->dev)));
 		/* Find the path by the hash. */
