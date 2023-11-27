8799becbc768abbaf392269e2d0d0c4853fdd345
Krishna chaitanya chundru <quic_krichai@quicinc.com>
2023-11-27 17:43:13 +0530

    PENDING: phy: qcom-qmp-pcie: Add support for keeping refclk always on
    In PCIe low power states like L1.1 or L1.2 the phy will stop
    supplying refclk to endpoint. If endpoint asserts clkreq to bring
    back link L0, then RC needs to provide refclk to endpoint.
    
    Some platforms with pcie switch fail to drive the clkreq signal to
    the host from the endpoints because of the switch board design.
    Due to that refclk needs to supplied to the endpoint always.
    
    Add a flag to keep refclk always supplied to endpoint.
    
    Change-Id: I7aaaa25589980ab38eb0fcc7a617c644ca8c656b
    Signed-off-by: Krishna chaitanya chundru <quic_krichai@quicinc.com>

--- drivers/phy/qualcomm/phy-qcom-qmp-pcie.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-pcie.c
@@ -36,6 +36,20 @@
 #include "phy-qcom-qmp-pcs-pcie-v6_20.h"
 #include "phy-qcom-qmp-pcie-qhp.h"
 
+/* QPHY_SW_RESET bit */
+#define SW_RESET				BIT(0)
+/* QPHY_POWER_DOWN_CONTROL */
+#define SW_PWRDN				BIT(0)
+#define REFCLK_DRV_DSBL				BIT(1)
+/* QPHY_START_CONTROL bits */
+#define SERDES_START				BIT(0)
+#define PCS_START				BIT(1)
+/* QPHY_PCS_STATUS bit */
+#define PHYSTATUS				BIT(6)
+#define PHYSTATUS_4_20				BIT(7)
+/* PCS_PCIE_ENDPOINT_REFCLK_CNTRL */
+#define EPCLK_ALWAYS_ON_EN			BIT(6)
+
 #define PHY_INIT_COMPLETE_TIMEOUT		10000
 
 /* set of registers with offsets different per-PHY */
@@ -2694,6 +2708,8 @@ struct qmp_pcie {
 	struct phy *phy;
 	int mode;
 
+	bool refclk_always_on;
+
 	struct clk_fixed_rate pipe_clk_fixed;
 	struct clk_fixed_rate aux_clk_fixed;
 };
@@ -3782,6 +3798,10 @@ static void qmp_pcie_init_registers(struct qmp_pcie *qmp, const struct qmp_phy_c
 	qmp_configure(qmp->dev, pcs, tbls->pcs, tbls->pcs_num);
 	qmp_configure(qmp->dev, pcs_misc, tbls->pcs_misc, tbls->pcs_misc_num);
 
+	if (qmp->refclk_always_on && cfg->regs[QPHY_PCS_ENDPOINT_REFCLK_CNTRL])
+		qphy_setbits(pcs_misc, cfg->regs[QPHY_PCS_ENDPOINT_REFCLK_CNTRL],
+			     EPCLK_ALWAYS_ON_EN);
+
 	if (cfg->lanes >= 4 && qmp->tcsr_4ln_config) {
 		qmp_configure(qmp->dev, serdes, cfg->serdes_4ln_tbl,
 			      cfg->serdes_4ln_num);
@@ -4374,6 +4394,12 @@ static int qmp_pcie_probe(struct platform_device *pdev)
 	}
 	if (ret)
 		goto err_node_put;
+
+	qmp->refclk_always_on = of_property_read_bool(dev->of_node, "qcom,refclk-always-on");
+	if (qmp->refclk_always_on && !qmp->cfg->regs[QPHY_PCS_ENDPOINT_REFCLK_CNTRL]) {
+		dev_err(dev, "refclk is always on is present but refclk cntrl offset is not present\n");
+		goto err_node_put;
+	}
 
 	ret = qmp_pcie_register_clocks(qmp, np);
 	if (ret)
