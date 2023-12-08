663e05625762082be10de776385ee0ad3719fda3
Christian Marangi <ansuelsmth@gmail.com>
2023-12-08 15:51:53 +0100

    UPSTREAM: net: phy: at803x: move specific at8031 probe mode check to dedicated probe
    Move specific at8031 probe mode check to dedicated probe to make
    at803x_probe more generic and keep code tidy.
    
    Change-Id: Icd9cc954e12c866c7cadfe92f39cf425c659213c
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Git-commit: 25d2ba94005fac18fe68878cddff59a67e115554
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Prashanth Reddy Baddam <praspras@qti.qualcomm.com>

--- drivers/net/phy/at803x.c
+++ drivers/net/phy/at803x.c
@@ -844,26 +844,6 @@ static int at803x_probe(struct phy_device *phydev)
 	if (ret)
 		return ret;
 
-	if (phydev->drv->phy_id == ATH8031_PHY_ID) {
-		int ccr = phy_read(phydev, AT803X_REG_CHIP_CONFIG);
-		int mode_cfg;
-
-		if (ccr < 0)
-			return ccr;
-		mode_cfg = ccr & AT803X_MODE_CFG_MASK;
-
-		switch (mode_cfg) {
-		case AT803X_MODE_CFG_BX1000_RGMII_50OHM:
-		case AT803X_MODE_CFG_BX1000_RGMII_75OHM:
-			priv->is_1000basex = true;
-			fallthrough;
-		case AT803X_MODE_CFG_FX100_RGMII_50OHM:
-		case AT803X_MODE_CFG_FX100_RGMII_75OHM:
-			priv->is_fiber = true;
-			break;
-		}
-	}
-
 	return 0;
 }
 
@@ -1584,25 +1564,46 @@ static int at8031_parse_dt(struct phy_device *phydev)
 
 static int at8031_probe(struct phy_device *phydev)
 {
+	struct at803x_priv *priv;
+	int mode_cfg;
+	int ccr;
 	int ret;
 
 	ret = at803x_probe(phydev);
 	if (ret)
-		return ret;
+    	return ret;
+  
+        priv = phydev->priv;
 
 	/* Only supported on AR8031/AR8033, the AR8030/AR8035 use strapping
 	 * options.
 	 */
 	ret = at8031_parse_dt(phydev);
 	if (ret)
-		return ret;
+    	return ret;
+
+	ccr = phy_read(phydev, AT803X_REG_CHIP_CONFIG);
+	if (ccr < 0)
+            return ccr;
+	mode_cfg = ccr & AT803X_MODE_CFG_MASK;
+
+	switch (mode_cfg) {
+	case AT803X_MODE_CFG_BX1000_RGMII_50OHM:
+	case AT803X_MODE_CFG_BX1000_RGMII_75OHM:
+            priv->is_1000basex = true;
+	    fallthrough;
+	case AT803X_MODE_CFG_FX100_RGMII_50OHM:
+	case AT803X_MODE_CFG_FX100_RGMII_75OHM:
+            priv->is_fiber = true;
+	    break;
+	}
 
 	/* Disable WoL in 1588 register which is enabled
 	 * by default
 	 */
 	return phy_modify_mmd(phydev, MDIO_MMD_PCS,
-			      AT803X_PHY_MMD3_WOL_CTRL,
-			      AT803X_WOL_EN, 0);
+                      AT803X_PHY_MMD3_WOL_CTRL,
+	              AT803X_WOL_EN, 0);
 }
 
 static int qca83xx_config_init(struct phy_device *phydev)
