bceabc75b3a12445c30ba928fdeae364286d8e19
Baochen Qiang <quic_bqiang@quicinc.com>
2024-02-01 11:58:30 +0800

    wifi: ath12k: enable 802.11 power save mode in station mode
    To reduce power consumption enable 802.11 power save mode in
    station mode. This allows both radio and CPU to sleep more.
    
    Only enable the mode on WCN7850, other chips don't support it
    for now.
    
    To test that power save mode is running, run below command and
    check there is no NULL Data frame seen by a sniffer:
            iw dev <inf> set power_save off
    
    And run below command, then check there is a NULL Data frame
    in sniffer:
            iw dev <inf> set power_save on
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20240201035830.2534-1-quic_bqiang@quicinc.com
    Signed-off-by: Yu Zhang(Yuriy) <yu.zhang@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -914,6 +914,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.max_mlo_peer = 256,
 
 		.otp_board_id_register = QCN9274_QFPROM_RAW_RFA_PDET_ROW13_LSB,
+
+		.supports_sta_ps = false,
 	},
 	{
 		.name = "wcn7850 hw2.0",
@@ -982,6 +984,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.max_mlo_peer = 32,
 
 		.otp_board_id_register = 0,
+
+		.supports_sta_ps = true,
 	},
 	{
 		.name = "qcn9274 hw2.0",
@@ -1048,6 +1052,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.max_mlo_peer = 256,
 
 		.otp_board_id_register = QCN9274_QFPROM_RAW_RFA_PDET_ROW13_LSB,
+
+		.supports_sta_ps = false,
 	},
 };
 
