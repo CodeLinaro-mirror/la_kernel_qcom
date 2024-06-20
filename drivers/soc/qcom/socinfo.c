decd4f1eb7867da49856a8c97f346de4647794a9
Jingyi Wang <quic_jingyw@quicinc.com>
2024-06-20 16:18:52 +0800

    PENDING: soc: qcom: socinfo: add QCS8300/QCS8275 SoC ID
    Add SoC ID for Qualcomm QCS8300/QCS8275.
    
    Change-Id: Ia178adcecde1c5c00f4a040c5b54102151a7b24e
    Signed-off-by: Jingyi Wang <quic_jingyw@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -449,6 +449,8 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(QCS8300) },
 	{ qcom_board_id(QCS8275) },
 	{ qcom_board_id(QCS9100) },
+	{ qcom_board_id(QCS8300) },
+	{ qcom_board_id(QCS8275) },
 };
 
 static const char *socinfo_machine(struct device *dev, unsigned int id)
