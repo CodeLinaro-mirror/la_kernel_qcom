2af5eb3c01de64a9541231640da9d5c184539885
Mao Jinlong <quic_jinlmao@quicinc.com>
2024-06-26 18:39:26 +0800

    FROMLIST: coresight: Add support to get preferred id for system trace sources
    Dynamic trace id was introduced in coresight subsystem, so trace id is
    allocated dynamically. However, some hardware ATB source has static trace
    id and it cannot be changed via software programming. For such source,
    it can call coresight_get_source_traceid to get the fixed trace id from
    device node and pass id to coresight_trace_id_get_system_id to reserve
    the id.
    
    Link: https://patchwork.kernel.org/project/linux-arm-kernel/patch/20240626060724.28862-3-quic_jinlmao@quicinc.com/
    Change-Id: I9d675a9555a4457c27b3fdbdbc4ec2091c4a7f88
    Signed-off-by: Mao Jinlong <quic_jinlmao@quicinc.com>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -663,6 +663,7 @@ void coresight_relaxed_write64(struct coresight_device *csdev,
 void coresight_write64(struct coresight_device *csdev, u64 val, u32 offset);
 
 extern int coresight_get_cpu(struct device *dev);
+extern int coresight_get_source_traceid(struct device *dev, u32 *id);
 extern const char *coresight_get_device_name(struct device *dev);
 
 struct coresight_platform_data *coresight_get_platform_data(struct device *dev);
