ac00c94b15eeccb8b29d26a53f2a34d3cf1dcf6e
Hardeep Sharma <quic_hardshar@quicinc.com>
2024-11-27 10:38:49 +0530

    drivers: soc: qcom: dcvs: fix some DCVS compile issue on 32 bit
    fix some DCVS compile issue on 32 bit.
    
    Change-Id: I268ba0f040e896c587b43d0253989d1ded802280
    Signed-off-by: Hardeep Sharma <quic_hardshar@quicinc.com>

--- drivers/soc/qcom/dcvs/memlat.c
+++ drivers/soc/qcom/dcvs/memlat.c
@@ -22,6 +22,7 @@
 #include <linux/irq.h>
 #include <linux/cpu_pm.h>
 #include <linux/cpu.h>
+#include <asm/cputype.h>
 #include <linux/of_fdt.h>
 #include <linux/of_device.h>
 #include <linux/mutex.h>
@@ -589,7 +590,7 @@ static ssize_t show_hlos_cpucp_offset(struct kobject *kobj,
 {
 	int ret;
 	const struct qcom_scmi_vendor_ops *ops =  memlat_data->ops;
-	uint64_t cpucp_ts, hlos_ts;
+	uint64_t cpucp_ts, hlos_ts, schd_clk;
 
 	if (!ops)
 		return -ENODEV;
@@ -600,7 +601,8 @@ static ssize_t show_hlos_cpucp_offset(struct kobject *kobj,
 		return ret;
 	}
 
-	hlos_ts = sched_clock()/1000;
+	schd_clk = sched_clock();
+	hlos_ts = do_div(schd_clk, 1000);
 
 	return scnprintf(buf, PAGE_SIZE, "%llu\n", le64_to_cpu(cpucp_ts) - hlos_ts);
 }
@@ -834,6 +836,8 @@ static void calculate_sampling_stats(void)
 	struct cpu_stats *stats;
 	struct cpu_ctrs *delta;
 	struct memlat_group *memlat_grp;
+	u64 freq, ipm;
+
 	ktime_t now = ktime_get();
 	s64 delta_us, update_us;
 
@@ -882,19 +886,24 @@ static void calculate_sampling_stats(void)
 			}
 		}
 
-		stats->freq_mhz = delta->common_ctrs[CYC_IDX] / delta_us;
+		freq = delta->common_ctrs[CYC_IDX];
+		do_div(freq, delta_us);
+		stats->freq_mhz = freq;
+
 		if (!memlat_data->common_ev_ids[FE_STALL_IDX])
 			stats->fe_stall_pct = 100;
-		else
+		else {
 			stats->fe_stall_pct = mult_frac(100,
-					       delta->common_ctrs[FE_STALL_IDX],
-					       delta->common_ctrs[CYC_IDX]);
+							(u32)delta->common_ctrs[FE_STALL_IDX],
+							(u32)delta->common_ctrs[CYC_IDX]);
+		}
 		if (!memlat_data->common_ev_ids[BE_STALL_IDX])
 			stats->be_stall_pct = 100;
-		else
+		else {
 			stats->be_stall_pct = mult_frac(100,
-					       delta->common_ctrs[BE_STALL_IDX],
-					       delta->common_ctrs[CYC_IDX]);
+							(u32)delta->common_ctrs[BE_STALL_IDX],
+							(u32)delta->common_ctrs[CYC_IDX]);
+			}
 		for (grp = 0; grp < MAX_MEMLAT_GRPS; grp++) {
 			memlat_grp = memlat_data->groups[grp];
 			if (!memlat_grp) {
@@ -902,18 +911,19 @@ static void calculate_sampling_stats(void)
 				stats->wb_pct[grp] = 0;
 				continue;
 			}
-			stats->ipm[grp] = delta->common_ctrs[INST_IDX];
-			if (delta->grp_ctrs[grp][MISS_IDX])
-				stats->ipm[grp] /=
-					delta->grp_ctrs[grp][MISS_IDX];
-
+			ipm = delta->common_ctrs[INST_IDX];
+			if (delta->grp_ctrs[grp][MISS_IDX]) {
+				do_div(ipm, delta->grp_ctrs[grp][MISS_IDX]);
+				stats->ipm[grp] = ipm;
+			}
 			if (!memlat_grp->grp_ev_ids[WB_IDX]
 					|| !memlat_grp->grp_ev_ids[ACC_IDX])
 				stats->wb_pct[grp] = 0;
-			else
+			else {
 				stats->wb_pct[grp] = mult_frac(100,
-						delta->grp_ctrs[grp][WB_IDX],
-						delta->grp_ctrs[grp][ACC_IDX]);
+						(u32)delta->grp_ctrs[grp][WB_IDX],
+						(u32)delta->grp_ctrs[grp][ACC_IDX]);
+			}
 
 			/* one meas event per memlat_group with group name */
 			trace_memlat_dev_meas(dev_name(memlat_grp->dev), cpu,
