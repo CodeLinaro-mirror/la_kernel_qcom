c08392ee5516525de417788b13112051671777c2
Hardeep Sharma <quic_hardshar@quicinc.com>
2024-12-05 16:23:40 +0530

    drivers: minidump: Fix some compilation errors for 32bit
    Fix some compilation errors when enabling minidump on arm32.
    
    Change-Id: I6689a8e05e9cd67ad7f925a698b490d62db8c8b5
    Signed-off-by: Hardeep Sharma <quic_hardshar@quicinc.com>

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -46,8 +46,9 @@
 
 #include <linux/sched/cputime.h>
 #include "../../../kernel/sched/sched.h"
+#if IS_ENABLED(CONFIG_SCHED_WALT)
 #include <linux/sched/walt.h>
-
+#endif
 #include <linux/kdebug.h>
 #include <linux/thread_info.h>
 #include <asm/ptrace.h>
@@ -181,17 +182,20 @@ static void register_kernel_sections(void)
 	struct md_region ksec_entry;
 	char *data_name = "KDATABSS";
 	char *rodata_name = "KROAIDATA";
+#ifdef CONFIG_SMP
 	size_t static_size;
 	void __percpu *base;
 	unsigned int cpu;
+#endif
 	void *_sdata, *__bss_stop;
 	void *start_ro, *end_ro;
 
 	_sdata = DEBUG_SYMBOL_LOOKUP(_sdata);
 	__bss_stop = DEBUG_SYMBOL_LOOKUP(__bss_stop);
+#ifdef CONFIG_SMP
 	base = DEBUG_SYMBOL_LOOKUP(__per_cpu_start);
 	static_size = (size_t)(DEBUG_SYMBOL_LOOKUP(__per_cpu_end) - base);
-
+#endif
 	strscpy(ksec_entry.name, data_name, sizeof(ksec_entry.name));
 	ksec_entry.virt_addr = (u64)_sdata;
 	ksec_entry.phys_addr = virt_to_phys(_sdata);
@@ -208,6 +212,7 @@ static void register_kernel_sections(void)
 	if (msm_minidump_add_region(&ksec_entry) < 0)
 		pr_err("Failed to add rodata section in Minidump\n");
 
+#ifdef CONFIG_SMP
 	/* Add percpu static sections */
 	for_each_possible_cpu(cpu) {
 		void *start = per_cpu_ptr(base, cpu);
@@ -221,6 +226,7 @@ static void register_kernel_sections(void)
 		if (msm_minidump_add_region(&ksec_entry) < 0)
 			pr_err("Failed to add percpu sections in Minidump\n");
 	}
+#endif
 }
 
 static inline bool in_stack_range(
@@ -262,9 +268,10 @@ void dump_stack_minidump(u64 sp)
 		return;
 
 	is_vmap_stack = IS_ENABLED(CONFIG_VMAP_STACK);
-
+#ifdef CONFIG_ARM64
 	if (sp < KIMAGE_VADDR || sp > -256UL)
 		sp = current_stack_pointer;
+#endif
 
 	/*
 	 * Since stacks are now allocated with vmalloc, the translation to
@@ -706,10 +713,19 @@ static void md_dump_task_info(struct task_struct *task, char *status,
 
 	se = &task->se;
 	if (task == curr) {
+#ifdef CONFIG_THREAD_INFO_IN_TASK
+#ifdef CONFIG_ARM64
 		seq_buf_printf(md_runq_seq_buf,
 			       "[status: curr] pid: %d preempt: %#llx\n",
 			       task_pid_nr(task),
 			       task->thread_info.preempt_count);
+#else
+		seq_buf_printf(md_runq_seq_buf,
+			       "[status: curr] pid: %d preempt: %#x\n",
+			       task_pid_nr(task),
+			       task->thread_info.preempt_count);
+#endif
+#endif
 		return;
 	}
 
@@ -917,7 +933,9 @@ static void md_dump_runqueues(void)
 		seq_buf_printf(md_runq_seq_buf, "%16lld", t->sched_info.last_queued);
 		seq_buf_printf(md_runq_seq_buf, "%16lld", t->sched_info.run_delay);
 		seq_buf_printf(md_runq_seq_buf, "%12ld", t->sched_info.pcount);
+#ifdef CONFIG_SMP
 		seq_buf_printf(md_runq_seq_buf, "%4d", t->on_cpu);
+#endif
 		seq_buf_printf(md_runq_seq_buf, "%5d", t->prio);
 		seq_buf_printf(md_runq_seq_buf, "%*s", 6, md_get_task_state(t));
 #if IS_ENABLED(CONFIG_SCHED_WALT)
@@ -1235,7 +1253,7 @@ static int register_vmap_mem(const char *name, void *virual_addr, size_t dump_le
 	int i = 0;
 
 	while (dump_len) {
-		to_dump = min(dump_len, PAGE_SIZE - offset_in_page(dump_addr));
+		to_dump = min(dump_len, (size_t)(PAGE_SIZE - offset_in_page(dump_addr)));
 		phys_addr = page_to_phys(vmalloc_to_page((const void *)dump_addr));
 		snprintf(entry_name, sizeof(entry_name), "%d_%s", i, name);
 		md_register_minidump_entry(entry_name, (u64)dump_addr, phys_addr, to_dump);
