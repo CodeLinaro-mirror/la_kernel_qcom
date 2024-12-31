arter97/clo_kernel_qcom

PENDING: drm/msm/dp: Add DisplayPort controller for QCS8300
The Qualcomm QCS8300 platform comes with a DisplayPort controller
with a different base offset than the previous SoCs,
add supoort for this in the DisplayPort driver.

Change-Id: I4f42fc07ddee834307b7611a66c84edf65db417b
Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>

drivers/gpu/drm/msm/dp/dp_display.c
@@ -130,6 +130,12 @@ struct msm_dp_desc {
 	bool wide_bus_en;
 };
 
+static const struct msm_dp_desc qcs8300_dp_descs[] = {
+	{ .io_start = 0xaf54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_en = true },
+	{ .io_start = 0xaf5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_en = true },
+	{}
+};
+
 static const struct msm_dp_desc sa8775p_dp_descs[] = {
 	{ .io_start = 0xaf54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_en = true },
 	{ .io_start = 0xaf5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_en = true },
@@ -185,6 +191,7 @@ static const struct msm_dp_desc sm8650_dp_descs[] = {
 };
 
 static const struct of_device_id dp_dt_match[] = {
+	{ .compatible = "qcom,qcs8300-dp", .data = &qcs8300_dp_descs },
 	{ .compatible = "qcom,sa8775p-dp", .data = &sa8775p_dp_descs },
 	{ .compatible = "qcom,sc7180-dp", .data = &sc7180_dp_descs },
 	{ .compatible = "qcom,sc7280-dp", .data = &sc7280_dp_descs },

CodeLinaro-mirror/la_kernel_qcom

drivers/gpu/drm/msm/dp/dp_display.c
@@ -119,6 +119,12 @@ struct msm_dp_desc {
 	bool wide_bus_supported;
 };
 
+static const struct msm_dp_desc qcs8300_dp_descs[] = {
+	{ .io_start = 0xaf54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true },
+	{ .io_start = 0xaf5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true },
+	{}
+};
+
 static const struct msm_dp_desc sa8775p_dp_descs[] = {
 	{ .io_start = 0xaf54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true },
 	{ .io_start = 0xaf5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true },
@@ -169,6 +175,7 @@ static const struct msm_dp_desc x1e80100_dp_descs[] = {
 };
 
 static const struct of_device_id dp_dt_match[] = {
+	{ .compatible = "qcom,qcs8300-dp", .data = &qcs8300_dp_descs },
 	{ .compatible = "qcom,sa8775p-dp", .data = &sa8775p_dp_descs },
 	{ .compatible = "qcom,sc7180-dp", .data = &sc7180_dp_descs },
 	{ .compatible = "qcom,sc7280-dp", .data = &sc7280_dp_descs },