CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

cfg80211: Add support for ML STA fallback association
In 6.6 kernel, when MLO STA downgrades itself to connect to legacy AP,
the connect response from driver does not contain any wdev->valid_links
information since the association is a legacy association.

To support this, add fallback_valid_links bitmap as part of connect
response as well as wdev to identify the link that has downgraded and
use the same for deletion of the link and other similar operations.

Change-Id: Ia5d89daec56445f715c9923baea6a4f154e6b99c
Signed-off-by: Abishek Ganapathy <quic_abishekg@quicinc.com>

net/wireless/nl80211.c
@@ -3932,6 +3932,41 @@ static int nl80211_send_iface(struct sk_buff *msg, u32 portid, u32 seq, int flag
 		}
 
 		nla_nest_end(msg, links);
+	} else if (wdev->fallback_valid_links) {
+		unsigned int link_id;
+		struct nlattr *links = nla_nest_start(msg,
+						      NL80211_ATTR_MLO_LINKS);
+
+		if (!links)
+			goto nla_put_failure;
+
+		for_each_fallback_valid_link(wdev, link_id) {
+			struct nlattr *link = nla_nest_start(msg, link_id + 1);
+			struct cfg80211_chan_def chandef = {};
+			int ret;
+
+			if (!link)
+				goto nla_put_failure;
+
+			if (nla_put_u8(msg, NL80211_ATTR_MLO_LINK_ID, link_id))
+				goto nla_put_failure;
+			ret = rdev_get_channel(rdev, wdev, link_id, &chandef);
+			if (ret == 0 && nl80211_send_chandef(msg, &chandef))
+				goto nla_put_failure;
+
+			if (rdev->ops->get_tx_power) {
+				int dbm, ret;
+
+				ret = rdev_get_tx_power(rdev, wdev, link_id, &dbm);
+				if (ret == 0 &&
+				    nla_put_u32(msg, NL80211_ATTR_WIPHY_TX_POWER_LEVEL,
+						DBM_TO_MBM(dbm)))
+					goto nla_put_failure;
+			}
+			nla_nest_end(msg, link);
+		}
+
+		nla_nest_end(msg, links);
 	} else {
 		if (rdev->ops->get_channel) {
 			struct cfg80211_chan_def chandef = {};
@@ -16680,18 +16715,27 @@ static int nl80211_pre_doit(const struct genl_split_ops *ops,
 			goto out_unlock;
 		}
 
-		/* MLO -> require valid link ID */
-		if (wdev->valid_links &&
+		/* MLO fallback to DBDC association */
+		if (wdev->fallback_valid_links &&
 		    (!link_id ||
-		     !(wdev->valid_links & BIT(nla_get_u8(link_id))))) {
+		     !(wdev->fallback_valid_links & BIT(nla_get_u8(link_id))))) {
 			err = -EINVAL;
 			goto out_unlock;
-		}
+		} else {
+			/* MLO -> require valid link ID */
+			if (wdev->valid_links &&
+			    (!link_id ||
+			     !(wdev->valid_links & BIT(nla_get_u8(link_id))))) {
+				err = -EINVAL;
+				goto out_unlock;
+			}
 
-		/* non-MLO -> no link ID attribute accepted */
-		if (!wdev->valid_links && link_id) {
-			err = -EINVAL;
-			goto out_unlock;
+			/* non-MLO -> no link ID attribute accepted */
+			if (!wdev->fallback_valid_links &&
+			    !wdev->valid_links && link_id) {
+				err = -EINVAL;
+				goto out_unlock;
+			}
 		}
 	}
 

CodeLinaro-mirror/la_kernel_qcom

Change-Id: I04316e7861b75b173e5752afff7d928a56db9561

net/wireless/nl80211.c
@@ -3985,6 +3985,41 @@ static int nl80211_send_iface(struct sk_buff *msg, u32 portid, u32 seq, int flag
 					DBM_TO_MBM(dbm)))
 				goto nla_put_failure;
 		}
+	} else if (wdev->fallback_valid_links) {
+		unsigned int link_id;
+		struct nlattr *links = nla_nest_start(msg,
+						      NL80211_ATTR_MLO_LINKS);
+
+		if (!links)
+			goto nla_put_failure;
+
+		for_each_fallback_valid_link(wdev, link_id) {
+			struct nlattr *link = nla_nest_start(msg, link_id + 1);
+			struct cfg80211_chan_def chandef = {};
+			int ret;
+
+			if (!link)
+				goto nla_put_failure;
+
+			if (nla_put_u8(msg, NL80211_ATTR_MLO_LINK_ID, link_id))
+				goto nla_put_failure;
+			ret = rdev_get_channel(rdev, wdev, link_id, &chandef);
+			if (ret == 0 && nl80211_send_chandef(msg, &chandef))
+				goto nla_put_failure;
+
+			if (rdev->ops->get_tx_power) {
+				int dbm, ret;
+
+				ret = rdev_get_tx_power_mlo(rdev, wdev, link_id, &dbm);
+				if (ret == 0 &&
+				    nla_put_u32(msg, NL80211_ATTR_WIPHY_TX_POWER_LEVEL,
+						DBM_TO_MBM(dbm)))
+					goto nla_put_failure;
+			}
+			nla_nest_end(msg, link);
+		}
+
+		nla_nest_end(msg, links);
 	}
 
 	genlmsg_end(msg, hdr);
@@ -16754,18 +16789,27 @@ static int nl80211_pre_doit(const struct genl_split_ops *ops,
 			goto out_unlock;
 		}
 
-		/* MLO -> require valid link ID */
-		if (wdev->valid_links &&
+		/* MLO fallback to DBDC association */
+		if (wdev->fallback_valid_links &&
 		    (!link_id ||
-		     !(wdev->valid_links & BIT(nla_get_u8(link_id))))) {
+		     !(wdev->fallback_valid_links & BIT(nla_get_u8(link_id))))) {
 			err = -EINVAL;
 			goto out_unlock;
-		}
+		} else {
+			/* MLO -> require valid link ID */
+			if (wdev->valid_links &&
+			    (!link_id ||
+			     !(wdev->valid_links & BIT(nla_get_u8(link_id))))) {
+				err = -EINVAL;
+				goto out_unlock;
+			}
 
-		/* non-MLO -> no link ID attribute accepted */
-		if (!wdev->valid_links && link_id) {
-			err = -EINVAL;
-			goto out_unlock;
+			/* non-MLO -> no link ID attribute accepted */
+			if (!wdev->fallback_valid_links &&
+			    !wdev->valid_links && link_id) {
+				err = -EINVAL;
+				goto out_unlock;
+			}
 		}
 	}
 