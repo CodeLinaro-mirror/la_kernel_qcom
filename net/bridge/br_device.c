caf-backup/quic_la_kernel_msm

Fixed the br_dev_xmit function to call Hy-Fi hooks
- Previously this wasn't calling the Hy-Fi hooks (meaning that all
  locally generated traffic was sent on a FDB selected port, and hence
  in a Hy-Fi environment, could be spread across all backhauls)
- Fixed to be the same as the banana kernel, and call the
  br_get_dst_hook_t

Change-Id: If5e964f385614beb5ebe9e1083ca947464165b9e
Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

net/bridge/br_device.c
@@ -34,6 +34,8 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 	struct net_bridge_mdb_entry *mdst;
 	struct pcpu_sw_netstats *brstats = this_cpu_ptr(br->stats);
 	u16 vid = 0;
+	struct net_bridge_port *pdst;
+	br_get_dst_hook_t *get_dst_hook;
 
 	rcu_read_lock();
 #ifdef CONFIG_BRIDGE_NETFILTER
@@ -56,6 +58,8 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 
 	if (!br_allowed_ingress(br, br_get_vlan_info(br), skb, &vid))
 		goto out;
+
+	get_dst_hook = rcu_dereference(br_get_dst_hook);
 
 	if (is_broadcast_ether_addr(dest))
 		br_flood_deliver(br, skb, false);
@@ -80,10 +84,20 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 			br_multicast_deliver(mdst, skb);
 		else
 			br_flood_deliver(br, skb, false);
-	} else if ((dst = __br_fdb_get(br, dest, vid)) != NULL)
-		br_deliver(dst->dst, skb);
-	else
-		br_flood_deliver(br, skb, true);
+	} else {
+		pdst = __br_get(get_dst_hook, NULL, NULL, &skb);
+		if (pdst) {
+			if (!skb)
+				goto out;
+			br_deliver(pdst, skb);
+		} else {
+			dst = __br_fdb_get(br, dest, vid);
+			if (dst)
+				br_deliver(dst->dst, skb);
+			else
+				br_flood_deliver(br, skb, true);
+		}
+	}
 
 out:
 	rcu_read_unlock();

CodeLinaro-mirror/la_kernel_qcom

Signed-off-by: Mughilan Ramajayam <mughilan@codeaurora.org>

net/bridge/br_device.c
@@ -37,6 +37,8 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 	struct net_bridge_vlan *vlan;
 	const unsigned char *dest;
 	u16 vid = 0;
+	struct net_bridge_port *pdst;
+	br_get_dst_hook_t *get_dst_hook;
 
 	memset(skb->cb, 0, sizeof(struct br_input_skb_cb));
 	br_tc_skb_miss_set(skb, false);
@@ -79,6 +81,8 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 				br_do_suppress_nd(skb, br, vid, NULL, msg);
 	}
 
+	get_dst_hook = rcu_dereference(br_get_dst_hook);
+
 	dest = eth_hdr(skb)->h_dest;
 	if (is_broadcast_ether_addr(dest)) {
 		br_flood(br, skb, BR_PKT_BROADCAST, false, true, vid);
@@ -103,11 +107,21 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 			br_multicast_flood(mdst, skb, brmctx, false, true);
 		else
 			br_flood(br, skb, BR_PKT_MULTICAST, false, true, vid);
-	} else if ((dst = br_fdb_find_rcu(br, dest, vid)) != NULL) {
-		br_forward(dst->dst, skb, false, true);
 	} else {
-		br_flood(br, skb, BR_PKT_UNICAST, false, true, vid);
+		pdst = __br_get(get_dst_hook, NULL, NULL, &skb);
+		if (pdst) {
+			if (!skb)
+				goto out;
+			br_forward(pdst, skb, false, true);
+		} else {
+			dst = br_fdb_find_rcu(br, dest, vid);
+			if (dst)
+				br_forward(dst->dst, skb, false, true);
+			else
+				br_flood(br, skb, BR_PKT_UNICAST, false, true, vid);
+		}
 	}
+
 out:
 	rcu_read_unlock();
 	return NETDEV_TX_OK;