CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

net:gre: Add API to check if a GRE netdevice is a fallback netdevice
Updated the existing API in gre.c to also handle L3 GRE netdevices.
Added new API in gre6.c to return if given netdevice matches fallback
netdevice.

Change-Id: I2c3a167582164e9fd17499ae26f6c0be8040df01
Signed-off-by: Karthik T S <quic_kartikts@quicinc.com>

net/ipv6/ip6_gre.c
@@ -740,6 +740,28 @@ static struct ip_tunnel_info *skb_tunnel_info_txcheck(struct sk_buff *skb)
 	return tun_info;
 }
 
+bool gre6_tunnel_is_fallback_dev(struct net_device *dev)
+{
+	struct net *net;
+	struct net_device *fb_tunnel_dev = NULL;
+	struct ip6gre_net *itn = NULL;
+
+	net = dev_net(dev);
+	if (!net)
+		return false;
+
+	if (dev->rtnl_link_ops && !strcmp(dev->rtnl_link_ops->kind, "ip6gre"))
+		itn = net_generic(net, ip6gre_net_id);
+
+	if (!itn)
+		return false;
+
+	fb_tunnel_dev = itn->fb_tunnel_dev;
+
+	return (fb_tunnel_dev == dev);
+}
+EXPORT_SYMBOL(gre6_tunnel_is_fallback_dev);
+
 static netdev_tx_t __gre6_xmit(struct sk_buff *skb,
 			       struct net_device *dev, __u8 dsfield,
 			       struct flowi6 *fl6, int encap_limit,

CodeLinaro-mirror/la_kernel_qcom

Signed-off-by: Kuruva, Jaya Krishna <quic_jkuruva@quicinc.com>

net/ipv6/ip6_gre.c
@@ -742,6 +742,28 @@ static struct ip_tunnel_info *skb_tunnel_info_txcheck(struct sk_buff *skb)
 	return tun_info;
 }
 
+bool gre6_tunnel_is_fallback_dev(struct net_device *dev)
+{
+	struct net *net;
+	struct net_device *fb_tunnel_dev = NULL;
+	struct ip6gre_net *itn = NULL;
+
+	net = dev_net(dev);
+	if (!net)
+		return false;
+
+	if (dev->rtnl_link_ops && !strcmp(dev->rtnl_link_ops->kind, "ip6gre"))
+		itn = net_generic(net, ip6gre_net_id);
+
+	if (!itn)
+		return false;
+
+	fb_tunnel_dev = itn->fb_tunnel_dev;
+
+	return (fb_tunnel_dev == dev);
+}
+EXPORT_SYMBOL_GPL(gre6_tunnel_is_fallback_dev);
+
 static netdev_tx_t __gre6_xmit(struct sk_buff *skb,
 			       struct net_device *dev, __u8 dsfield,
 			       struct flowi6 *fl6, int encap_limit,