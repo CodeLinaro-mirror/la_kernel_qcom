PeridotSupremacy/kernel_xiaomi_sm8635

soc: qcom: hgsl: add more debug infos
add more runtime debug infos, include active contexts
and isync information.

Change-Id: I07d7c417fcf17d81e4b76a00fa7ee1d7f9ad899d
Signed-off-by: Hui Li <quic_hul@quicinc.com>

drivers/soc/qcom/hgsl/hgsl_debugfs.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -9,6 +9,128 @@
 
 #include "hgsl.h"
 #include "hgsl_debugfs.h"
+
+#define HGSL_SHADOW_TS_INFO_LENGTH (64)
+static int hgsl_stat_show(struct seq_file *s, void *unused)
+{
+	struct qcom_hgsl *hgsl = s->private;
+	struct hgsl_hsync_timeline *tl;
+	struct hgsl_isync_timeline *cur;
+	struct hgsl_context *ctxt;
+	struct doorbell_queue *dbq;
+	struct doorbell_context_queue *dbcq;
+	uint32_t ts = 0, idr;
+	int i = 0, found = 0, ret, dev_hnd = GSL_HANDLE_DEV0;
+	char shadow_ts_info[HGSL_SHADOW_TS_INFO_LENGTH];
+	struct hgsl_active_wait *wait;
+
+	seq_printf(s, "DEVICE INFO:\n"
+		"{ default_iocoherency=%d, db_off=%d, total_mem_size=%lld; }\n",
+		hgsl->default_iocoherency, hgsl->db_off,
+		atomic64_read(&hgsl->total_mem_size));
+
+	seq_printf(s, "\n%s\n%s\n", "ACTIVE ISYNCS:", "{");
+	spin_lock(&hgsl->isync_timeline_lock);
+	idr_for_each_entry(&hgsl->isync_timeline_idr, cur, idr) {
+		seq_printf(s,
+			"    %s: { t_context=0x%llx, signaled_ts=%u, flags=0x%x, 64bit=%u; }\n",
+			cur->name, cur->context, cur->last_ts, cur->flags, cur->is64bits);
+		found = 1;
+	}
+	spin_unlock(&hgsl->isync_timeline_lock);
+	if (!found)
+		seq_puts(s, "    null\n");
+	seq_puts(s, "}\n");
+
+	found = 0;
+	seq_printf(s, "\n%s\n%s\n", "ACTIVE WAITING CONTEXTS:", "{");
+	spin_lock(&hgsl->active_wait_lock);
+	list_for_each_entry(wait, &hgsl->active_wait_list, head) {
+		seq_printf(s, "    { context_id=%u, wait_timestamp=%u; }\n",
+			wait->ctxt->context_id, wait->timestamp);
+		found = 1;
+	}
+	spin_unlock(&hgsl->active_wait_lock);
+	if (!found)
+		seq_puts(s, "    null\n");
+	seq_puts(s, "}\n");
+
+	seq_printf(s, "\n%s\n", "ACTIVE CONTEXTS:");
+	for (; dev_hnd < (HGSL_DEVICE_NUM + 1); dev_hnd++) {
+		for (; i < HGSL_CONTEXT_NUM; i++) {
+			ctxt = hgsl_get_context(hgsl, dev_hnd, i);
+			if (!ctxt)
+				continue;
+
+			memset(shadow_ts_info, 0, sizeof(shadow_ts_info));
+			if (ctxt->shadow_ts) {
+				mutex_lock(&ctxt->lock);
+				ts = get_context_retired_ts(ctxt);
+				mutex_unlock(&ctxt->lock);
+				snprintf(shadow_ts_info, sizeof(shadow_ts_info),
+					"shadow ts enabled");
+			} else {
+				struct hgsl_ioctl_read_ts_params param;
+
+				param.devhandle = dev_hnd;
+				param.ctxthandle = i;
+				param.type = GSL_TIMESTAMP_RETIRED;
+				ret = hgsl_hyp_read_timestamp(&hgsl->global_hyp, &param);
+				if (ret)
+					ts = param.timestamp;
+				snprintf(shadow_ts_info, sizeof(shadow_ts_info),
+					"shadow ts disabled, read ts from BE %s",
+					ret ? "failure" : "success");
+			}
+			seq_printf(s, "ID=%3u: {\n", ctxt->context_id);
+			seq_printf(s,
+				"    pid=%d, devhandle=%u, is_fe_shadow=%u, in_destroy=%u, ",
+				ctxt->pid, ctxt->devhandle, ctxt->is_fe_shadow, ctxt->in_destroy);
+			seq_printf(s,
+				"dbq_info=0x%x, flags=0x%x, tcsr_idx=%d, db_signal=%u;\n",
+				ctxt->dbq_info, ctxt->flags, ctxt->tcsr_idx, ctxt->db_signal);
+			seq_printf(s,
+				"    queued_ts=%u, last_ts=%u, retired_ts=%u;\n",
+				ctxt->queued_ts, ctxt->last_ts, ts);
+			seq_printf(s, "    %s;\n", shadow_ts_info);
+
+			dbq = ctxt->dbq;
+			if (dbq)
+				seq_printf(s,
+					"    dbq idx=%u: {\n"
+					"        state=%u, tcsr_idx=%d, ibdesc_max_size=%u, seq_num=%d;\n"
+					"    }\n",
+					dbq->dbq_idx, dbq->state, dbq->tcsr_idx,
+					dbq->ibdesc_max_size, atomic_read(&dbq->seq_num));
+
+			dbcq = ctxt->dbcq;
+			if (dbcq) {
+				seq_printf(s,
+					"    dbcq irq_bit_idx=%d: {\n", dbcq->irq_bit_idx);
+				seq_printf(s,
+					"        db_signal=%u, queue_size=0x%x, indirect_ib_ts=%u, ",
+					dbcq->db_signal, dbcq->queue_size, dbcq->indirect_ib_ts);
+				seq_printf(s, "seq_num=%u, dbcq_export_id=%u;\n",
+					dbcq->seq_num, ctxt->dbcq_export_id);
+				seq_puts(s, "    }\n");
+			}
+
+			tl = ctxt->timeline;
+			if (tl)
+				seq_printf(s,
+					"    timeline=%s: {\n"
+					"        fcontext=0x%llx, signaled_ts=%u;\n"
+					"    }\n",
+					tl->name, tl->fence_context, tl->last_ts);
+
+			seq_puts(s, "}\n");
+			hgsl_put_context(ctxt);
+		}
+	}
+
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(hgsl_stat);
 
 static int hgsl_client_mem_show(struct seq_file *s, void *unused)
 {
@@ -187,6 +309,9 @@ void hgsl_debugfs_init(struct platform_device *pdev)
 
 	hgsl->debugfs = root;
 	hgsl->clients_debugfs = debugfs_create_dir("clients", root);
+
+	hgsl->debugfs_stat = debugfs_create_file("stat", 0444,
+		root, hgsl, &hgsl_stat_fops);
 }
 
 void hgsl_debugfs_release(struct platform_device *pdev)

CodeLinaro-mirror/la_kernel_qcom

drivers/soc/qcom/hgsl/hgsl_debugfs.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  */
 
@@ -9,6 +9,128 @@
 
 #include "hgsl.h"
 #include "hgsl_debugfs.h"
+
+#define HGSL_SHADOW_TS_INFO_LENGTH (64)
+static int hgsl_stat_show(struct seq_file *s, void *unused)
+{
+	struct qcom_hgsl *hgsl = s->private;
+	struct hgsl_hsync_timeline *tl;
+	struct hgsl_isync_timeline *cur;
+	struct hgsl_context *ctxt;
+	struct doorbell_queue *dbq;
+	struct doorbell_context_queue *dbcq;
+	uint32_t ts = 0, idr;
+	int i = 0, found = 0, ret, dev_hnd = GSL_HANDLE_DEV0;
+	char shadow_ts_info[HGSL_SHADOW_TS_INFO_LENGTH];
+	struct hgsl_active_wait *wait;
+
+	seq_printf(s, "DEVICE INFO:\n"
+		"{ default_iocoherency=%d, db_off=%d, total_mem_size=%lld; }\n",
+		hgsl->default_iocoherency, hgsl->db_off,
+		atomic64_read(&hgsl->total_mem_size));
+
+	seq_printf(s, "\n%s\n%s\n", "ACTIVE ISYNCS:", "{");
+	spin_lock(&hgsl->isync_timeline_lock);
+	idr_for_each_entry(&hgsl->isync_timeline_idr, cur, idr) {
+		seq_printf(s,
+			"    %s: { t_context=0x%llx, signaled_ts=%u, flags=0x%x, 64bit=%u; }\n",
+			cur->name, cur->context, cur->last_ts, cur->flags, cur->is64bits);
+		found = 1;
+	}
+	spin_unlock(&hgsl->isync_timeline_lock);
+	if (!found)
+		seq_puts(s, "    null\n");
+	seq_puts(s, "}\n");
+
+	found = 0;
+	seq_printf(s, "\n%s\n%s\n", "ACTIVE WAITING CONTEXTS:", "{");
+	spin_lock(&hgsl->active_wait_lock);
+	list_for_each_entry(wait, &hgsl->active_wait_list, head) {
+		seq_printf(s, "    { context_id=%u, wait_timestamp=%u; }\n",
+			wait->ctxt->context_id, wait->timestamp);
+		found = 1;
+	}
+	spin_unlock(&hgsl->active_wait_lock);
+	if (!found)
+		seq_puts(s, "    null\n");
+	seq_puts(s, "}\n");
+
+	seq_printf(s, "\n%s\n", "ACTIVE CONTEXTS:");
+	for (; dev_hnd < (HGSL_DEVICE_NUM + 1); dev_hnd++) {
+		for (; i < HGSL_CONTEXT_NUM; i++) {
+			ctxt = hgsl_get_context(hgsl, dev_hnd, i);
+			if (!ctxt)
+				continue;
+
+			memset(shadow_ts_info, 0, sizeof(shadow_ts_info));
+			if (ctxt->shadow_ts) {
+				mutex_lock(&ctxt->lock);
+				ts = get_context_retired_ts(ctxt);
+				mutex_unlock(&ctxt->lock);
+				snprintf(shadow_ts_info, sizeof(shadow_ts_info),
+					"shadow ts enabled");
+			} else {
+				struct hgsl_ioctl_read_ts_params param;
+
+				param.devhandle = dev_hnd;
+				param.ctxthandle = i;
+				param.type = GSL_TIMESTAMP_RETIRED;
+				ret = hgsl_hyp_read_timestamp(&hgsl->global_hyp, &param);
+				if (ret)
+					ts = param.timestamp;
+				snprintf(shadow_ts_info, sizeof(shadow_ts_info),
+					"shadow ts disabled, read ts from BE %s",
+					ret ? "failure" : "success");
+			}
+			seq_printf(s, "ID=%3u: {\n", ctxt->context_id);
+			seq_printf(s,
+				"    pid=%d, devhandle=%u, is_fe_shadow=%u, in_destroy=%u, ",
+				ctxt->pid, ctxt->devhandle, ctxt->is_fe_shadow, ctxt->in_destroy);
+			seq_printf(s,
+				"dbq_info=0x%x, flags=0x%x, tcsr_idx=%d, db_signal=%u;\n",
+				ctxt->dbq_info, ctxt->flags, ctxt->tcsr_idx, ctxt->db_signal);
+			seq_printf(s,
+				"    queued_ts=%u, last_ts=%u, retired_ts=%u;\n",
+				ctxt->queued_ts, ctxt->last_ts, ts);
+			seq_printf(s, "    %s;\n", shadow_ts_info);
+
+			dbq = ctxt->dbq;
+			if (dbq)
+				seq_printf(s,
+					"    dbq idx=%u: {\n"
+					"        state=%u, tcsr_idx=%d, ibdesc_max_size=%u, seq_num=%d;\n"
+					"    }\n",
+					dbq->dbq_idx, dbq->state, dbq->tcsr_idx,
+					dbq->ibdesc_max_size, atomic_read(&dbq->seq_num));
+
+			dbcq = ctxt->dbcq;
+			if (dbcq) {
+				seq_printf(s,
+					"    dbcq irq_idx=%d: {\n", dbcq->irq_idx);
+				seq_printf(s,
+					"        db_signal=%u, queue_size=0x%x, indirect_ib_ts=%u, ",
+					dbcq->db_signal, dbcq->queue_size, dbcq->indirect_ib_ts);
+				seq_printf(s, "seq_num=%u, dbcq_export_id=%u;\n",
+					dbcq->seq_num, ctxt->dbcq_export_id);
+				seq_puts(s, "    }\n");
+			}
+
+			tl = ctxt->timeline;
+			if (tl)
+				seq_printf(s,
+					"    timeline=%s: {\n"
+					"        fcontext=0x%llx, signaled_ts=%u;\n"
+					"    }\n",
+					tl->name, tl->fence_context, tl->last_ts);
+
+			seq_puts(s, "}\n");
+			hgsl_put_context(ctxt);
+		}
+	}
+
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(hgsl_stat);
 
 static int hgsl_client_mem_show(struct seq_file *s, void *unused)
 {
@@ -138,6 +260,9 @@ void hgsl_debugfs_init(struct platform_device *pdev)
 
 	hgsl->debugfs = root;
 	hgsl->clients_debugfs = debugfs_create_dir("clients", root);
+
+	hgsl->debugfs_stat = debugfs_create_file("stat", 0444,
+		root, hgsl, &hgsl_stat_fops);
 }
 
 void hgsl_debugfs_release(struct platform_device *pdev)