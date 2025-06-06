92930c7603909d25edcbb471a3bf4ef0dd62f700
Meenu Raja Sundaram <mrajasun@qti.qualcomm.com>
2025-06-06 17:43:34 +0530

    soc-repo: Use glink_cma_dev to access qcom_glink from virtio-glink driver
    Use glink_cma_dev to access qcom_glink from virtio-glink driver.
    
    Change-Id: I0e810cfade3e93a97a5b8b0d5dcb133dcaa69e87
    Signed-off-by: Meenu Raja Sundaram <mrajasun@qti.qualcomm.com>

--- drivers/rpmsg/qcom_glink_cma_core.c
+++ drivers/rpmsg/qcom_glink_cma_core.c
@@ -298,7 +298,7 @@ static irqreturn_t qcom_glink_cma_intr(int irq, void *data)
 
 	return IRQ_HANDLED;
 }
-struct qcom_glink *qcom_glink_cma_register(struct device *parent, struct device_node *node,
+struct glink_cma_dev *qcom_glink_cma_register(struct device *parent, struct device_node *node,
 					struct glink_cma_config *config)
 {
 	struct glink_cma_dev *gdev;
@@ -376,7 +376,7 @@ struct qcom_glink *qcom_glink_cma_register(struct device *parent, struct device_
 	gdev->glink = glink;
 
 	GLINK_CMA_DEBUG_LOG(gdev->glink_cma_ilc, "success");
-	return glink;
+	return gdev;
 
 err_free_mbox:
 	mbox_free_channel(gdev->mbox_chan);
@@ -390,17 +390,15 @@ err_put_dev:
 }
 EXPORT_SYMBOL_GPL(qcom_glink_cma_register);
 
-void qcom_glink_cma_unregister(struct qcom_glink *glink)
-{
-	struct glink_cma_dev *gdev;
 
-	if (!glink)
+void qcom_glink_cma_unregister(struct glink_cma_dev *gdev)
+{
+	if (!gdev)
 		return;
 
-	gdev = container_of(&glink, struct glink_cma_dev, glink);
 	disable_irq(gdev->irq);
 
-	qcom_glink_native_remove(glink);
+	qcom_glink_native_remove(gdev->glink);
 
 	mbox_free_channel(gdev->mbox_chan);
 	device_unregister(&gdev->dev);
