525787221109d03a0a16c7e9704f20e7671e643e
Arun Gopal Kondaveeti <quic_arungopa@quicinc.com>
2025-06-12 14:38:46 +0530

    soc: qcom: ps_hold power off type to warm reset
    In non-psci targets with secure boot devices for adb reboot
    edl case need to set ps_hold reset type to warm reset.
    
    Change-Id: Ib25d37a4f6f79bcf03b66e7e2e8fb2b00a8e53c0
    Signed-off-by: Arun Gopal Kondaveeti <quic_arungopa@quicinc.com>
    Signed-off-by: Mandar Mahesh Kamble <quic_maheshka@quicinc.com>

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2020, 2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022,2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/delay.h>
@@ -17,6 +17,10 @@
 #include <linux/panic_notifier.h>
 #include <linux/qcom_scm.h>
 #include <soc/qcom/minidump.h>
+#ifdef CONFIG_INPUT_QPNP_POWER_ON
+#include <linux/input/qpnp-power-on.h>
+#endif
+
 
 enum qcom_download_dest {
 	QCOM_DOWNLOAD_DEST_UNKNOWN = -1,
@@ -260,6 +264,9 @@ static int qcom_dload_panic(struct notifier_block *this, unsigned long event,
 {
 	struct qcom_dload *poweroff = container_of(this, struct qcom_dload,
 						     panic_nb);
+	#ifdef CONFIG_INPUT_QPNP_POWER_ON
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+	#endif
 	poweroff->in_panic = true;
 	if (enable_dump)
 		msm_enable_dump_mode(true);
@@ -290,9 +297,12 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 	poweroff->in_reboot = true;
 	set_download_mode(QCOM_DOWNLOAD_NODUMP);
 	if (cmd) {
-		if (!strcmp(cmd, "edl"))
+		if (!strcmp(cmd, "edl")) {
+			#ifdef CONFIG_INPUT_QPNP_POWER_ON
+			qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+			#endif
 			set_download_mode(QCOM_DOWNLOAD_EDL);
-		else if (!strcmp(cmd, "qcom_dload"))
+		} else if (!strcmp(cmd, "qcom_dload"))
 			msm_enable_dump_mode(true);
 	}
 
