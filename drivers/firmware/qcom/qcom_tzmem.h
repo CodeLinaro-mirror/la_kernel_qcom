e1022a4d4bac7b549fcf318daf69deebe4d9f848
Kuldeep Singh <kuldsing@qti.qualcomm.com>
2025-08-13 14:50:13 +0530

    firmware: qcom: qtee_shmbridge: Use tzmem interfaces
    Migrate qtee_shmbridge to use qcom_tzmem interfaces and make shmbridge
    thin layer.
    
    Expose qcom_tzmem_using_shm_bridge so that qtee_shmbridge doesn't make
    another scm call.
    
    Change-Id: I9690f8dd855acf5ecfdcf6ec3f94bbb75aad986f
    Signed-off-by: Kuldeep Singh <kuldsing@qti.qualcomm.com>

--- drivers/firmware/qcom/qcom_tzmem.h
+++ drivers/firmware/qcom/qcom_tzmem.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (C) 2023-2024 Linaro Ltd.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef __QCOM_TZMEM_PRIV_H
@@ -8,6 +9,22 @@
 
 struct device;
 
+bool qcom_tzmem_get_status(void);
 int qcom_tzmem_enable(struct device *dev);
+
+#if IS_ENABLED(CONFIG_QCOM_TZMEM_MODE_SHMBRIDGE)
+int32_t qcom_tzmem_query(phys_addr_t paddr);
+int qcom_tzmem_shm_bridge_create_with_vmid(phys_addr_t paddr, size_t size, u32 vmid, u64 *handle);
+#else
+static inline int32_t qcom_tzmem_query(phys_addr_t paddr)
+{
+	return 0;
+}
+static inline int qcom_tzmem_shm_bridge_create_with_vmid(phys_addr_t paddr, size_t size,
+							 u32 vmid, u64 *handle)
+{
+	return 0;
+}
+#endif /* CONFIG_QCOM_TZMEM_MODE_SHMBRIDGE */
 
 #endif /* __QCOM_TZMEM_PRIV_H */
