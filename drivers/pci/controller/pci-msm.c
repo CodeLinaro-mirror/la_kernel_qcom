realme-kernel-opensource/realme_GT8pro-AndroidB-kernel-source

pci: msm: Add checks to avoid NP exception during late probe
If pcie probe is delayed, *pcie_dev remains NULL. Now if cnss driver
calls target_link_speed() or enumerate(), it will result in NP
exception. This patch adds checks to bail out if pcie_dev is
uninitialized (due to delayed driver probe).

Change-Id: Ia767def0e6be6d8e67f03effd5f24e2e4f9ed69c
Signed-off-by: Sushrut Shree Trivedi <quic_sushruts@quicinc.com>
Signed-off-by: Saranya R <quic_sarar@quicinc.com>

drivers/pci/controller/pci-msm.c
@@ -6572,18 +6572,26 @@ int msm_pcie_enumerate(u32 rc_idx)
 	int domain_nr;
 	struct msm_pcie_dev_t *dev = msm_pcie_dev[rc_idx];
 	struct pci_dev *pcidev = NULL;
-	struct pci_host_bridge *bridge = dev->bridge;
+	struct pci_host_bridge *bridge;
 	u32 ids, vendor_id, device_id;
 	struct pci_config_window *cfg;
 	const struct pci_ecam_ops *ecam_ops = &msm_pcie_ops;
 	struct resource_entry *bus;
 	LIST_HEAD(res);
 
+	if (!dev) {
+		PCIE_DBG(dev,
+			"PCIe: RC%d: has not been successfully probed yet\n",
+			rc_idx);
+		return -EPROBE_DEFER;
+	}
+
 	mutex_lock(&dev->enumerate_lock);
 
 	PCIE_DBG(dev, "Enumerate RC%d\n", rc_idx);
 
 	dev->fmd_enable = false;
+	bridge = dev->bridge;
 	if (!dev->driver_probed) {
 		PCIE_DBG(dev,
 			"PCIe: RC%d: has not been successfully probed yet\n",
@@ -9118,7 +9126,7 @@ int msm_pcie_set_target_link_speed(u32 rc_idx, u32 target_link_speed,
 
 	pcie_dev = msm_pcie_dev[rc_idx];
 
-	if (!pcie_dev->driver_probed) {
+	if (!pcie_dev || !pcie_dev->driver_probed) {
 		PCIE_DBG(pcie_dev,
 			"PCIe: RC%d: has not been successfully probed yet\n",
 			pcie_dev->rc_idx);

CodeLinaro-mirror/la_kernel_qcom

Signed-off-by: Mayank Rana <mayank.rana@oss.qualcomm.com>

drivers/pci/controller/pci-msm.c
@@ -6958,6 +6958,11 @@ int msm_pcie_enumerate(u32 rc_idx)
 	struct resource_entry *bus;
 	LIST_HEAD(res);
 
+	if (!dev) {
+		pr_err("PCIe: RC%d: has not been successfully probed yet\n", rc_idx);
+		return -EPROBE_DEFER;
+	}
+
 	mutex_lock(&dev->enumerate_lock);
 
 	PCIE_DBG(dev, "Enumerate RC%d\n", rc_idx);
@@ -9806,11 +9811,8 @@ int msm_pcie_set_target_link_speed(u32 rc_idx, u32 target_link_speed,
 	}
 
 	pcie_dev = msm_pcie_dev[rc_idx];
-
-	if (!pcie_dev->drv_ready) {
-		PCIE_DBG(pcie_dev,
-			"PCIe: RC%d: has not been successfully probed yet\n",
-			pcie_dev->rc_idx);
+	if (!pcie_dev || !pcie_dev->drv_ready) {
+		pr_err("PCIe: RC(%d) has not been successfully probed yet\n", rc_idx);
 		return -EPROBE_DEFER;
 	}
 