b0a114c055678416b2f9aa7776642a1d95558b73
Sairam Vamsi <quic_sairvams@quicinc.com>
2025-09-08 13:57:13 +0530

    arm: dt-bindings: Add soc-id support in dt-bindings
    Add colibri target SoC id support in dt-bindings.
    
    Change-Id: I76c779ddaf49fa57fc801319eefbee5b1f86824b
    Signed-off-by: Sairam Vamsi <quic_sairvams@quicinc.com>

--- include/dt-bindings/arm/qcom,ids.h
+++ include/dt-bindings/arm/qcom,ids.h
@@ -302,6 +302,7 @@
 #define QCOM_ID_CHORA			724
 #define QCOM_ID_PIKACHU			736
 #define QCOM_ID_CANOEPSG		743
+#define QCOM_ID_COLIBRI			756
 
 /*
  * The board type and revision information, used by Qualcomm bootloaders and
