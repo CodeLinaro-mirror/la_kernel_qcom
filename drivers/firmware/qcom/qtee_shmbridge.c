3d4ab6e45e65c28f823b8dd34cefe18af0712ab9
Kuldeep Singh <kuldsing@qti.qualcomm.com>
2025-10-07 21:08:36 +0530

    firmware: qcom: qcom_tzmem: Add support for registration of multiple VMs
    Currently qcom_tzmem driver support registration of single non-secure VM
    which is limited usage of actual shmbridge functionality.
    
    Add support to enable registration with multiple VMs and ensure to
    register bridge for pool creation with self owner bit to work correctly
    with secondary VMs.
    
    Change-Id: I0dc5d13ec574ad8469a3f950e19e7019ad26f275
    Signed-off-by: Kuldeep Singh <kuldsing@qti.qualcomm.com>
    (cherry picked from commit be6ce1e2e47a135e649d8bc8590d2023ed2e780b)

--- drivers/firmware/qcom/qtee_shmbridge.c
+++ drivers/firmware/qcom/qtee_shmbridge.c
@@ -195,8 +195,8 @@ int32_t qtee_shmbridge_register(phys_addr_t paddr, size_t size, uint32_t *ns_vmi
 	if (ret)
 		goto bridge_exist;
 
-	ret = qcom_tzmem_shm_bridge_create_with_vmid(paddr, size, ns_vmid_list[0],
-							OTHERS, &qtee_handle);
+	ret = qcom_tzmem_shm_bridge_create_with_vmid(paddr, size, ns_vmid_list,
+			ns_vm_perm_list, ns_vmid_num, tz_perm, OTHERS, &qtee_handle);
 	if (ret)
 		goto exit;
 
@@ -211,7 +211,6 @@ bridge_exist:
 exit:
 	mutex_unlock(&bridge_list_head.lock);
 	return ret;
-
 }
 EXPORT_SYMBOL(qtee_shmbridge_register);
 
