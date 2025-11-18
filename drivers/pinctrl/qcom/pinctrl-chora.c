c81b62f73cd9abdbcf77791c9142646a7364557b
Mukesh Pilaniya <mukesh.pilaniya@oss.qualcomm.com>
2025-11-18 15:47:09 +0530

    pinctrl: Add support for Chora VM
    Add pinctrl support for chora VM.
    
    Change-Id: I5b99de40e10f96365e3eb13f81a46c0dba9429c8
    Signed-off-by: Mukesh Pilaniya <mukesh.pilaniya@oss.qualcomm.com>

--- drivers/pinctrl/qcom/pinctrl-chora.c
+++ drivers/pinctrl/qcom/pinctrl-chora.c
@@ -2123,8 +2123,20 @@ static const struct msm_pinctrl_soc_data chora_tlmm = {
 	.egpio_func = 11,
 };
 
+static const struct msm_pinctrl_soc_data chora_vm_tlmm = {
+	.pins = chora_pins,
+	.npins = ARRAY_SIZE(chora_pins),
+	.functions = chora_functions,
+	.nfunctions = ARRAY_SIZE(chora_functions),
+	.groups = chora_groups,
+	.ngroups = ARRAY_SIZE(chora_groups),
+	.ngpios = 152,
+	.egpio_func = 11,
+};
+
 static const struct of_device_id chora_tlmm_of_match[] = {
 	{ .compatible = "qcom,chora-tlmm", .data = &chora_tlmm },
+	{ .compatible = "qcom,chora-vm-tlmm", .data = &chora_vm_tlmm },
 	{},
 };
 
