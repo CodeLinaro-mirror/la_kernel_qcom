4a68b70fc2c4aae0db845b0fac55c1219cf5a699
Abhishek Chauhan <abchauha@qti.qualcomm.com>
2025-12-09 14:58:32 -0800

    PENDING: net: stmmac: Avoid access of register after suspend
    Xmit of packet and suspending of device can happen in parallel
    and in different context. It can happen that packet can arrive from
    the network stack while the suspend is in progress. In such cases
    we need to check the netdev device presence before we try to access
    the tail pointer register to avoid access of emac register when the
    clocks are disabled.
    
    Change-Id: I4c4b9ca71a1160bfbccadf583b4e02237e2f3453
    Signed-off-by: Abhishek Chauhan <abchauha@qti.qualcomm.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4228,7 +4228,11 @@ static void stmmac_flush_tx_descriptors(struct stmmac_priv *priv, int queue)
 	 */
 	wmb();
 
-	WARN_ON(!netif_device_present(priv->dev));
+	/* Suspend and xmit are happening in parallel context */
+	if (unlikely(!netif_device_present(priv->dev))) {
+		WARN_ON(1);
+		return;
+	}
 
 	tx_q->tx_tail_addr = tx_q->dma_tx_phy + (tx_q->cur_tx * desc_size);
 	stmmac_set_tx_tail_ptr(priv, priv->ioaddr, tx_q->tx_tail_addr, queue);
