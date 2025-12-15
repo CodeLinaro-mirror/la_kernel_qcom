cd2d2a7aef00bf2cdb3873532cfcceec2582b59f
Saranya R <sarar@qti.qualcomm.com>
2025-12-15 14:46:01 +0530

    usb: phy: snps: Enable the clocks before accessing EUD_EN reg
    Enable the clocks before accessing EUD_EN reg in order to
    prevent unclocked access.
    
    Change-Id: I018524d392548d43d430639948e8e341ac9135eb
    Suggested-by: Prashanth K <prashanth.k@oss.qualcomm.com>
    Signed-off-by: Saranya R <sarar@qti.qualcomm.com>

--- drivers/usb/phy/phy-msm-snps-hs.c
+++ drivers/usb/phy/phy-msm-snps-hs.c
@@ -1268,10 +1268,13 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 	 * kernel boot till USB phy driver is initialized based on cable status,
 	 * keep LDOs on here.
 	 */
-	if (phy->eud_enable_reg && readl_relaxed(phy->eud_enable_reg)) {
-		msm_hsphy_modeled_d3_to_d0(phy);
-		msm_hsphy_enable_power(phy, true);
+	if (phy->eud_enable_reg) {
 		msm_hsphy_enable_clocks(phy, true);
+		if (readl_relaxed(phy->eud_enable_reg)) {
+			msm_hsphy_modeled_d3_to_d0(phy);
+			msm_hsphy_enable_power(phy, true);
+		} else
+			msm_hsphy_enable_clocks(phy, false);
 	}
 
 	/* Placed at the end to ensure the probe is complete */
