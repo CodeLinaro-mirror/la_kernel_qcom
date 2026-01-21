3562749f83b5c86315237b276ea00e09d3e90733
Jun Zhang <quic_juzhan@quicinc.com>
2026-01-21 11:04:29 +0800

    hgsl: Update memdesc to align with userspace gsl
    Update the definition of gsl_memdesc_t to align with the userspace gsl.
    
    Change-Id: I787769e15a59d807c73d2840fda61a1c7d30e848
    Signed-off-by: Jun Zhang <quic_juzhan@quicinc.com>

--- drivers/soc/qcom/hgsl/hgsl_types.h
+++ drivers/soc/qcom/hgsl/hgsl_types.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2006-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef __HGSL_TYPES_H
@@ -147,6 +147,9 @@ struct gsl_memdesc_t {
 		uintptr_t    priv;
 		uint64_t     priv64;
 	};
+
+	void                 *placed_addr;
+	int                  reserve_placed;
 };
 
 struct gsl_command_buffer_object_t {
