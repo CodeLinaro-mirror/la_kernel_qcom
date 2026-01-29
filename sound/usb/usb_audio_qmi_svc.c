28284985ede03b9c1efc32c1a474c68392a2016d
Manish Nagar <mnagar@qti.qualcomm.com>
2026-01-29 14:43:05 +0530

    sound: usb: qmi: fix NULL pointer dereference in check_valid_request
    The pointer 'subs->cur_audiofmt' is checked for NULL earlieri
    in the info_idx_from_ifnum where it is used in a ternary operator
    to derive 'info_idx'. Then the pointer can be NULL.
    
    However, in the error handling block immediately following, if
    'req_msg->enable' is set and '*info_idx' is negative, the code
    unconditionally dereferences 'subs->cur_audiofmt' to print the
    interface number in an error log.
    
    If 'subs->cur_audiofmt' is NULL, this results potential NULL
    pointer dereference.
    
    Fix this by validating the pointer before accessing 'subs->cur_audiofmt->iface'
    within the logging statement.
    
    Change-Id: Ib0023685d688b8682fd405a1c430acae08bbc5cc
    Signed-off-by: Manish Nagar <mnagar@qti.qualcomm.com>

--- sound/usb/usb_audio_qmi_svc.c
+++ sound/usb/usb_audio_qmi_svc.c
@@ -1487,8 +1487,12 @@ static int check_valid_request(struct qmi_uaudio_stream_req_msg_v01 *req_msg,
 	}
 
 	if (req_msg->enable && (*info_idx < 0)) {
-		uaudio_err("interface# %d already in use card# %d\n",
+		if (subs->cur_audiofmt) {
+			uaudio_err("interface# %d already in use card# %d\n",
 				subs->cur_audiofmt->iface, pcm_card_num);
+		} else {
+			uaudio_err("Invalid audio format for card# %d\n", pcm_card_num);
+		}
 		return -EBUSY;
 	}
 
