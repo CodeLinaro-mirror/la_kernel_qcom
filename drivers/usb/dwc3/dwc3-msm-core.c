eae3cf6f5ad5d79bc9467a2a305a90e3e4df4121
Udipto Goswami <udipto.goswami@oss.qualcomm.com>
2026-03-24 13:09:55 +0530

    usb: audio: qmi: Add support for accessing QSRAM register via dwc3_msm
    Add support for retrieving the QSRAM resource from the dwc3-msm (MDWC)
    node using a device‑tree–based lookup. Since usb_audio_qmi_svc is
    already a platform driver, it now scans the MDWC node during probe to
    obtain the QSRAM resource directly, eliminating the need for explicit
    ioremap.
    
    Change-Id: I3f9c2c9cd11992a0fdc6c67f813ce2012da52007
    Signed-off-by: Udipto Goswami <udipto.goswami@oss.qualcomm.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -60,6 +60,7 @@
 #include "drivers/usb/dwc3/debug.h"
 #include "drivers/usb/host/xhci.h"
 #include "debug-ipc.h"
+#include <trace/hooks/usb.h>
 
 #define NUM_LOG_PAGES   12
 
@@ -529,6 +530,7 @@ struct dwc3_msm {
 	void __iomem *base;
 	void __iomem *tcsr_dyn_en_dis;
 	void __iomem *ahb2phy_base;
+	struct qsram_xhci __iomem *qsram;
 	phys_addr_t reg_phys;
 	struct platform_device	*dwc3;
 	struct dma_iommu_mapping *iommu_map;
@@ -6729,6 +6731,8 @@ static int dwc3_msm_parse_params(struct platform_device *pdev, struct device_nod
 		goto err;
 	}
 
+	mdwc->qsram = (struct qsram_xhci __iomem *)(mdwc->base + QSRAM_BASE_OFFSET);
+
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "tcsr_dyn_en_dis");
 	if (res) {
 		mdwc->tcsr_dyn_en_dis = devm_ioremap(&pdev->dev, res->start,
@@ -7982,6 +7986,26 @@ static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 
 	return 0;
 }
+
+struct qsram_xhci __iomem *dwc3_msm_get_qsram(struct device *dev)
+{
+	struct platform_device *pdev;
+	struct dwc3_msm *mdwc;
+
+	if (!dev)
+		return NULL;
+
+	pdev = to_platform_device(dev);
+	if (!pdev)
+		return NULL;
+
+	mdwc = platform_get_drvdata(pdev);
+	if (!mdwc)
+		return NULL;
+
+	return mdwc->qsram;
+}
+EXPORT_SYMBOL_GPL(dwc3_msm_get_qsram);
 
 /**
  * dwc3_otg_sm_work - workqueue function.
