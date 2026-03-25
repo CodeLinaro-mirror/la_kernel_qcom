bfebd80492f7975c7f54bddf39f2e319a8e0d46e
Bharat Goud Bommena <bbommena@qti.qualcomm.com>
2026-03-25 13:30:51 -0700

    net: qrtr: Fix race condition with MHI -EAGAIN
    There could be a chance when mhi ring is empty and qrtr-mhi
    still waiting for a completion event from ul callback.
    This change will fix the race completion race.
    
    Change-Id: I82c19e1647db2a448615d7ba58e8a522e338c990
    Signed-off-by: Bharat Goud Bommena <bbommena@qti.qualcomm.com>

--- net/qrtr/mhi.c
+++ net/qrtr/mhi.c
@@ -115,9 +115,9 @@ static int qcom_mhi_qrtr_send(struct qrtr_endpoint *ep, struct sk_buff *skb)
 	}
 
 	do {
+		reinit_completion(&qdev->ringfull);
 		rc = __qcom_mhi_qrtr_send(ep, skb);
 		if (rc == -EAGAIN) {
-			reinit_completion(&qdev->ringfull);
 			if (READ_ONCE(qdev->abort_tx)) {
 				if (skb->sk)
 					sock_put(skb->sk);
