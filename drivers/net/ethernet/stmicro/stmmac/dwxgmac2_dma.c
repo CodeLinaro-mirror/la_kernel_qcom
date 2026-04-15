04d472e6d2e1e2cb0aa4a049b153ad1b3236012b
Sarosh Hasan <sarohasa@qti.qualcomm.com>
2026-04-16 01:06:07 +0530

    net: stmmac: Disable VPRE when L2 split is enabled
    Disable VPRE when L2 split is active on HDMA platforms.
    When L2 split mode (SPLM=01) is active and the platform uses HDMA,
    clear the VPRE bit (bit 8). VPRE=1 allows the MAC to accept
    frames with variable-length preambles. With L2 split active, a
    variable-preamble frame shifts the effective start of the Ethernet
    header.So with VPRE=0, keeping the L2 split offset deterministic.
    
    Change-Id: I67c3ddd05aa66677a192cbef153f90063418b90e
    Signed-off-by: Sarosh Hasan <sarohasa@qti.qualcomm.com>

--- drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
+++ drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: (GPL-2.0 OR MIT)
 /*
  * Copyright (c) 2018 Synopsys, Inc. and/or its affiliates.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * stmmac XGMAC support.
  */
 
@@ -614,6 +615,24 @@ static void dwxgmac2_set_bfsize(struct stmmac_priv *priv, void __iomem *ioaddr,
 	writel(value, ioaddr + XGMAC_DMA_CH_RX_CONTROL(dwxgmac_addrs, chan));
 }
 
+static void dwxgmac2_set_splm(struct stmmac_priv *priv, void __iomem *ioaddr,
+			      u32 mode)
+{
+	u32 value;
+
+	value = readl(ioaddr + XGMAC_EXT_CFG1);
+	value |= XGMAC_CONFIG1_SPLM(mode);
+	if (mode == XGMAC_SPLM_L2)
+		value |= XGMAC_CONFIG1_SAVE_EN;
+	writel(value, ioaddr + XGMAC_EXT_CFG1);
+
+	if (mode == XGMAC_SPLM_L2) {
+		value = readl(ioaddr + XGMAC_EXT_CFG0);
+		value &= ~XGMAC_EXT_CFG0_VPRE;
+		writel(value, ioaddr + XGMAC_EXT_CFG0);
+	}
+}
+
 static void dwxgmac2_enable_sph(struct stmmac_priv *priv, void __iomem *ioaddr,
 				bool en, u32 chan)
 {
@@ -624,10 +643,7 @@ static void dwxgmac2_enable_sph(struct stmmac_priv *priv, void __iomem *ioaddr,
 	value |= XGMAC_CONFIG_HDSMS_256; /* Segment max 256 bytes */
 	writel(value, ioaddr + XGMAC_RX_CONFIG);
 
-	value = readl(ioaddr + XGMAC_EXT_CFG1);
-	value |= XGMAC_CONFIG1_SPLM(1);
-	value |= XGMAC_CONFIG1_SAVE_EN;
-	writel(value, ioaddr + XGMAC_EXT_CFG1);
+	dwxgmac2_set_splm(priv, ioaddr, XGMAC_SPLM_L2);
 
 	value = readl(ioaddr + XGMAC_DMA_CH_CONTROL(dwxgmac_addrs, chan));
 	if (en)
