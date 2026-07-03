fe7f029343ccfaa8050c2f3d7cb9d6da8b5a7a1b
Hongmei Xu <hongmeix@qti.qualcomm.com>
2026-07-03 15:01:48 +0800

    PENDING: net: stmmac: Fix interrupt storm after stmmac reset
    Issue:
       After trigger the TSOPES fusa error for several times,
       the network broken, there is interrupt storm on dma
       tx_rx-0 irq, the storm is the RI/TI interrupts.
    Root cause:
       In stmmac reset procedure, only common irq is disabled,
       per-channel irq is not disabled. As we moved from sbd
       interrupt to per-channel interrupt, so the storm happened.
    
    Change-Id: I2e506720c5cb09bdaba81db892abd0da4e052791
    Signed-off-by: Hongmei Xu <hongmeix@qti.qualcomm.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7220,6 +7220,23 @@ static const struct net_device_ops stmmac_netdev_ops = {
 	.ndo_xsk_wakeup = stmmac_xsk_wakeup,
 };
 
+static void stmmac_disable_perch_irq(struct stmmac_priv *priv)
+{
+	int i = 0;
+	u32 maxq;
+
+	maxq = max(priv->plat->rx_queues_to_use, priv->plat->tx_queues_to_use);
+
+	for (i = 0; i < maxq; i++) {
+		if (i >= STMMAC_CH_MAX)
+			break;
+		if (priv->tx_rx_irq[i] <= 0)
+			continue;
+
+		disable_irq(priv->tx_rx_irq[i]);
+	}
+}
+
 static void stmmac_reset_subtask(struct stmmac_priv *priv)
 {
 	if (!test_and_clear_bit(STMMAC_RESET_REQUESTED, &priv->state))
@@ -7235,6 +7252,7 @@ static void stmmac_reset_subtask(struct stmmac_priv *priv)
 		usleep_range(1000, 2000);
 
 	disable_irq(priv->dev->irq);
+	stmmac_disable_perch_irq(priv);
 	set_bit(STMMAC_DOWN, &priv->state);
 	dev_close(priv->dev);
 	dev_open(priv->dev, NULL);
