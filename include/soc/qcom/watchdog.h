ae0cd54d7543d680e154cb32bf745dcfebd922db
Samuel Lee <samuelee@qti.qualcomm.com>
2026-07-16 15:38:37 +0900

    qcom_wdog_core: avoid infinite loop when wdog driver is not fully initialized
    HQX platform, vdev handles GVM wdog through internal timer.
    early crash (qcom_wdt_trigger_bite) during GVM wdog driver is
    initializing, GVM can stuck in infinite loop condition because
    vdev timer is not properly set.
    add initialized flag to avoid this condition.
    
    Change-Id: Id61a669e1d7c86d389984598258464e28ab9aac5
    Signed-off-by: Samuel Lee <samuelee@qti.qualcomm.com>

--- include/soc/qcom/watchdog.h
+++ include/soc/qcom/watchdog.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef _SOC_QCOM_WATCHDOG_H_
@@ -135,6 +135,7 @@ struct msm_watchdog_data {
 	spinlock_t freeze_lock;
 	struct timer_list user_pet_timer;
 	bool hibernate;
+	bool initialized;
 };
 
 extern void qcom_wdt_trigger_bite(void);
